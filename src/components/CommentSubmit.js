72e354617550eff045cedd2fb550f5c0fd9fa7a5
waseemlinuxfreakz <waseem.linuxfreakz@gmail.com>
2022-06-23 14:52:19 +0500

    new updates
    all changes dates

--- src/components/CommentSubmit.js
+++ src/components/CommentSubmit.js
@@ -13,8 +13,8 @@ function CommentSubmit() {
                 <br />
                 <CommentSuccess/>
                 <br />
-                <div class="text-center">
-                    <a href="#" class="nextBtn">View</a>
+                <div className="text-center">
+                    <a href="#" className="nextBtn">View</a>
                 </div>
             </div>
         </section>
